6a41a8b5a917fff2d00e512cc34059934b6d15e3
pacruz02 <pacruz02@ucm.es>
2026-04-07 16:56:03 +0200

    arreglo del lobbyControler

--- src/main/java/es/ucm/fdi/iw/controller/LobbyController.java
+++ src/main/java/es/ucm/fdi/iw/controller/LobbyController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -18,6 +19,13 @@ import java.util.List;
 
 @Controller
 public class LobbyController {
+
+    @ModelAttribute
+    public void populateModel(HttpSession session, Model model) {
+        for (String name : new String[] { "u", "url", "ws", "topics" }) {
+            model.addAttribute(name, session.getAttribute(name));
+        }
+    }
 
     @Autowired
     private LobbyService lobbyService;
