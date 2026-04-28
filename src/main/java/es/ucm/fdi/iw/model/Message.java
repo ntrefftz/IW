5dadb96a1dfa69a4be1a1b43f3976adadf1eeb45
Jafifa1605 <javfig01@ucm.es>
2026-04-28 13:43:28 +0200

    Arreglo rápido mientras arreglo las namedqueries de Message

--- src/main/java/es/ucm/fdi/iw/model/Message.java
+++ src/main/java/es/ucm/fdi/iw/model/Message.java
@@ -25,9 +25,9 @@ import lombok.AllArgsConstructor;
  */
 @Entity
 @NamedQueries({
-	@NamedQuery(name="Message.countUnread",
-	query="SELECT COUNT(m) FROM Message m "
-			+ "WHERE m.recipient.id = :userId AND m.dateRead = null")
+	// @NamedQuery(name="Message.searchByLobby",
+	// query="SELECT COUNT(m) FROM Message m "
+	// 		+ "WHERE m.recipient.id = :userId AND m.dateRead = null")
 })
 @Data
 public class Message implements Transferable<Message.Transfer> {
